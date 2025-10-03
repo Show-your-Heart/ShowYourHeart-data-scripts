2ae8fc93382132eb7ba68f557aa6dfe6ee82fc27
Jordi Isidro Llobet <mail@jordiisidrollobet.tech>
2025-10-03 11:04:58 +0200

    answers_calc

--- daily/models/answers_calc_agg.sql
+++ daily/models/answers_calc_agg.sql
@@ -16,7 +16,7 @@ select id_campaign, campaign_name, "year", previous_campaign_id, id_survey, surv
 		when 2 then '"N/B"'
 		end::varchar,',')||']' end as str_gender
 , case when count(distinct gender)>0 then '['||string_agg(value,',')||']' else string_agg(value,'') end as str_value
-from {{ref('data_results')}}
+from {{ref('answers_calc')}}
 group by id_campaign, campaign_name, "year", previous_campaign_id, id_survey, survey_created_at, survey_updated_at, status, id_method, active, method_name, method_description, id_user, user_name, user_surname, user_email, id_organization, organization_name, vat_number, id_methods_section, method_section_title, method_order, method_level, path_order, sort_value, id_indicator, project_id, indicator_name, indicator_description, is_direct_indicator, indicator_category, indicator_data_type, indicator_unit
 
 
