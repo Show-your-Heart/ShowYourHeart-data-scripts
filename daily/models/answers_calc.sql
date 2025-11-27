c8c5a2f7b60ae7126e0557fdf0e6008aefffef80
Jordi Isidro Llobet <mail@jordiisidrollobet.tech>
2025-11-27 09:06:43 +0100

    afegit frances i projectes

--- daily/models/answers_calc.sql
+++ daily/models/answers_calc.sql
@@ -7,13 +7,13 @@
 
 with recursive method_section_hieriarchy as (
 select distinct s.id
-    , s.title, s.title_en, s.title_ca, s.title_gl, s.title_eu, s.title_es, s.title_nl
+    , s.title, s.title_en, s.title_ca, s.title_gl, s.title_eu, s.title_es, s.title_nl, s.title_fr
     , s.order, s.method_id, s.parent_id, 1 as lvl, cast(s.order as text) as path_order
 from {{ source('dwhpublic', 'syh_methods_section')}} s
 where parent_id is null
 union all
 select distinct s.id
-    , s.title, s.title_en, s.title_ca, s.title_gl, s.title_eu, s.title_es, s.title_nl
+    , s.title, s.title_en, s.title_ca, s.title_gl, s.title_eu, s.title_es, s.title_nl, s.title_fr
     , s.order, s.method_id, s.parent_id, ms.lvl+1 as lvl, ms.path_order || '.' || lpad(s.order::varchar,2,'0') AS path_order
 from {{ source('dwhpublic', 'syh_methods_section')}} s
 	join method_section_hieriarchy ms on ms.id=s.parent_id
@@ -22,44 +22,52 @@ select c.id as id_campaign
         , c.name as campaign_name
         , c.name_en as campaign_name_en, c.name_ca as campaign_name_ca, c.name_gl as campaign_name_gl
         , c.name_eu as campaign_name_eu, c.name_es as campaign_name_es, c.name_nl as campaign_name_nl
+        , c.name_fr as campaign_name_fr
         , c.year, c.previous_campaign_id
     , s.id as id_survey, s.created_at as survey_created_at, s.updated_at as survey_updated_at, s.status
     , m.id as id_method, m.active
         , m.name as method_name
         , m.name_en as method_name_en, m.name_ca as method_name_ca, m.name_gl as method_name_gl
         , m.name_eu as method_name_eu, m.name_es as method_name_es, m.name_nl as method_name_nl
+        , m.name_fr as method_name_fr
         , m.description as method_description
         , m.description_en as method_description_en, m.description_ca as method_description_ca
         , m.description_gl as method_description_gl, m.description_eu as method_description_eu
         , m.description_es as method_description_es, m.description_nl as method_description_nl
+        , m.description_fr as method_description_fr
     , u.id as id_user, u.name as user_name, u.surnames as user_surname, u.email as user_email
     , o.id as id_organization, o.name as organization_name, o.vat_number --TODO afegir més camps
     , null::uuid as id_methods_section
-        ,'Indirect indicator'::varchar(500) as method_section_title
-        ,'Indirect indicator'::varchar(500) as method_section_title_en
-        ,'Indicator indirecte'::varchar(500) as method_section_title_ca
-        ,'Indirect indicator'::varchar(500) as method_section_title_gl
-        ,'Indirect indicator'::varchar(500) as method_section_title_eu
-        ,'Indicador indirecto'::varchar(500) as method_section_title_es
+        , 'Indirect indicator'::varchar(500) as method_section_title
+        , 'Indirect indicator'::varchar(500) as method_section_title_en
+        , 'Indicator indirecte'::varchar(500) as method_section_title_ca
+        , 'Indirect indicator'::varchar(500) as method_section_title_gl
+        , 'Indirect indicator'::varchar(500) as method_section_title_eu
+        , 'Indicador indirecto'::varchar(500) as method_section_title_es
         , 'Indirect indicator'::varchar(500) as method_section_title_nl
+        , 'Indirect indicator'::varchar(500) as method_section_title_fr
         , 999 as method_order,1 as method_level, '9999.01' as path_order
         , 999 as sort_value
     , i.id as id_indicator, i.code as indicator_code
         , i.name as indicator_name
         , i.name_en as indicator_name_en, i.name_ca as indicator_name_ca, i.name_gl as indicator_name_gl
         , i.name_eu as indicator_name_eu, i.name_es as indicator_name_es, i.name_nl as indicator_name_nl
+        , i.name_fr as indicator_name_fr
         , i.description as indicator_description
         , i.description_en as indicator_description_en, i.description_ca as indicator_description_ca
         , i.description_gl as indicator_description_gl, i.description_eu as indicator_description_eu
         , i.description_es as indicator_description_es, i.description_nl as indicator_description_nl
+        , i.description_fr as indicator_description_fr
     	, i.is_direct_indicator, i.category as indicator_category, i.data_type as indicator_data_type, i.unit as indicator_unit
     , ir.id as id_indicatorresult, ir.gender
         , case when replace(ir.value, ' ', '')=',' or  replace(ir.value, ' ', '')='' then null
             when ir.value like '[%' and ir.value not like '%]' then null
             else replace(regexp_replace(ir.value, ',\s*\]', ']', 'g'), '"', '')
           end as value
+    , pr.id as id_project, pr.name as project_name
 from {{ source('dwhpublic', 'syh_methods_campaign')}} c
     join {{ source('dwhpublic', 'syh_methods_survey')}} s on s.campaign_id=c.id
+    left join {{ source('dwhpublic', 'syh_organizations_project')}} pr on s.project_id=pr.id
     join {{ source('dwhpublic', 'syh_methods_method')}} m on s.method_id=m.id
     join {{ source('dwhpublic', 'syh_organizations_organization')}} o on s.organization_id=o.id
     join {{ source('dwhpublic', 'syh_users_user')}} u on s.user_id=u.id
@@ -81,16 +89,19 @@ select distinct c.id as id_campaign
         , c.name as campaign_name
         , c.name_en as campaign_name_en, c.name_ca as campaign_name_ca, c.name_gl as campaign_name_gl
         , c.name_eu as campaign_name_eu, c.name_es as campaign_name_es, c.name_nl as campaign_name_nl
+        , c.name_fr as campaign_name_fr
         , c.year, c.previous_campaign_id
     , s.id as id_survey, s.created_at as survey_created_at, s.updated_at as survey_updated_at, s.status
     , m.id as id_method, m.active
         , m.name as method_name
         , m.name_en as method_name_en, m.name_ca as method_name_ca, m.name_gl as method_name_gl
         , m.name_eu as method_name_eu, m.name_es as method_name_es, m.name_nl as method_name_nl
+        , m.name_fr as method_name_fr
         , m.description as method_description
         , m.description_en as method_description_en, m.description_ca as method_description_ca
         , m.description_gl as method_description_gl, m.description_eu as method_description_eu
         , m.description_es as method_description_es, m.description_nl as method_description_nl
+        , m.description_fr as method_description_fr
     , u.id as id_user, u.name as user_name, u.surnames as user_surname, u.email as user_email
     , o.id as id_organization, o.name as organization_name, o.vat_number --TODO afegir més camps
     , h.id as id_methods_section
@@ -98,18 +109,22 @@ select distinct c.id as id_campaign
         , h.title_en as method_section_title_en, h.title_ca as method_section_title_ca
         , h.title_gl as method_section_title_gl, h.title_eu as method_section_title_eu
         , h.title_es as method_section_title_es, h.title_nl as method_section_title_nl
+        , h.title_fr as method_section_title_fr
         , h.order as method_order, h.lvl as method_level, h.path_order
     , si.sort_value
     , null::uuid as id_indicator, null as indicator_code
         , null as indicator_name
         , null as indicator_name_en, null as indicator_name_ca, null as indicator_name_gl
         , null as indicator_name_eu, null as indicator_name_es, null as indicator_name_nl
+        , null as indicator_name_fr
         , null as indicator_description
         , null as indicator_description_en, null as indicator_description_ca
         , null as indicator_description_gl, null as indicator_description_eu
         , null as indicator_description_es, null as indicator_description_nl
+        , null as indicator_description_fr
     	, true as is_direct_indicator, null as indicator_category, null as indicator_data_type, null as indicator_unit
     , null::uuid as id_indicatorresult, null::int as gender, null as value
+    , null::uuid as id_project, null as project_name
 from {{ source('dwhpublic', 'syh_methods_campaign')}} c
     join {{ source('dwhpublic', 'syh_methods_survey')}} s on s.campaign_id=c.id
     join {{ source('dwhpublic', 'syh_methods_method')}} m on s.method_id=m.id
@@ -131,16 +146,19 @@ select distinct c.id as id_campaign
         , c.name as campaign_name
         , c.name_en as campaign_name_en, c.name_ca as campaign_name_ca, c.name_gl as campaign_name_gl
         , c.name_eu as campaign_name_eu, c.name_es as campaign_name_es, c.name_nl as campaign_name_nl
+        , c.name_fr as campaign_name_fr
         , c.year, c.previous_campaign_id
     , s.id as id_survey, s.created_at as survey_created_at, s.updated_at as survey_updated_at, s.status
     , m.id as id_method, m.active
         , m.name as method_name
         , m.name_en as method_name_en, m.name_ca as method_name_ca, m.name_gl as method_name_gl
         , m.name_eu as method_name_eu, m.name_es as method_name_es, m.name_nl as method_name_nl
+        , m.name_nl as method_name_fr
         , m.description as method_description
         , m.description_en as method_description_en, m.description_ca as method_description_ca
         , m.description_gl as method_description_gl, m.description_eu as method_description_eu
         , m.description_es as method_description_es, m.description_nl as method_description_nl
+        , m.description_nl as method_description_fr
     , u.id as id_user, u.name as user_name, u.surnames as user_surname, u.email as user_email
     , o.id as id_organization, o.name as organization_name, o.vat_number --TODO afegir més camps
     , h.id as id_methods_section
@@ -148,18 +166,22 @@ select distinct c.id as id_campaign
         , h.title_en as method_section_title_en, h.title_ca as method_section_title_ca
         , h.title_gl as method_section_title_gl, h.title_eu as method_section_title_eu
         , h.title_es as method_section_title_es, h.title_nl as method_section_title_nl
+        , h.title_fr as method_section_title_fr
         , h.order as method_order, h.lvl as method_level, h.path_order
     , si.sort_value
     , null::uuid as id_indicator, null as indicator_code
         , null as indicator_name
         , null as indicator_name_en, null as indicator_name_ca, null as indicator_name_gl
         , null as indicator_name_eu, null as indicator_name_es, null as indicator_name_nl
+        , null as indicator_name_fr
         , null as indicator_description
         , null as indicator_description_en, null as indicator_description_ca
         , null as indicator_description_gl, null as indicator_description_eu
         , null as indicator_description_es, null as indicator_description_nl
+        , null as indicator_description_fr
     	, false as is_direct_indicator, null as indicator_category, null as indicator_data_type, null as indicator_unit
     , null::uuid as id_indicatorresult, null::int as gender, null as value
+    , null::uuid as id_project, null as project_name
 from {{ source('dwhpublic', 'syh_methods_campaign')}} c
     join {{ source('dwhpublic', 'syh_methods_survey')}} s on s.campaign_id=c.id
     join {{ source('dwhpublic', 'syh_methods_method')}} m on s.method_id=m.id
--- daily/models/answers_calc_agg.sql
+++ daily/models/answers_calc_agg.sql
@@ -5,80 +5,87 @@
 ) }}
 
 select id_campaign
-, max(campaign_name) as campaign_name
-, max(campaign_name_en) as campaign_name_en
-, max(campaign_name_ca) as campaign_name_ca
-, max(campaign_name_es) as campaign_name_es
-, max(campaign_name_eu) as campaign_name_eu
-, max(campaign_name_gl) as campaign_name_gl
-, max(campaign_name_nl) as campaign_name_nl
-, max("year") as "year", max(previous_campaign_id::varchar)::uuid as previous_campaign_id
-, id_survey, max(survey_created_at) as survey_created_at, max(survey_updated_at) as survey_updated_at
-, max(status) as status
-, id_method, bool_or(active) as active
-, max(method_name) as method_name
-, max(method_name_en) as method_name_en
-, max(method_name_ca) as method_name_ca
-, max(method_name_es) as method_name_es
-, max(method_name_eu) as method_name_eu
-, max(method_name_gl) as method_name_gl
-, max(method_name_nl) as method_name_nl
-, max(method_description) as method_description
-, max(method_description_en) as method_description_en
-, max(method_description_ca) as method_description_ca
-, max(method_description_es) as method_description_es
-, max(method_description_eu) as method_description_eu
-, max(method_description_gl) as method_description_gl
-, max(method_description_nl) as method_description_nl
-, id_user, max(user_name) as user_name, max(user_surname) as user_surname, max(user_email) as user_email
-, id_organization, max(organization_name) as organization_name, max(vat_number) as vat_number
-, id_methods_section
-, max(method_section_title) as method_section_title
-, max(method_section_title_en) as method_section_title_en
-, max(method_section_title_ca) as method_section_title_ca
-, max(method_section_title_es) as method_section_title_es
-, max(method_section_title_eu) as method_section_title_eu
-, max(method_section_title_gl) as method_section_title_gl
-, max(method_section_title_nl) as method_section_title_nl
-, max(method_order) as method_order, max(method_level) as method_level, max(path_order) as path_order
-, max(sort_value) as sort_value
-, id_indicator, indicator_code
-, max(indicator_name) as indicator_name
-, max(indicator_name_en) as indicator_name_en
-, max(indicator_name_ca) as indicator_name_ca
-, max(indicator_name_es) as indicator_name_es
-, max(indicator_name_eu) as indicator_name_eu
-, max(indicator_name_gl) as indicator_name_gl
-, max(indicator_name_nl) as indicator_name_nl
-, max(indicator_description) as indicator_description
-, max(indicator_description_en) as indicator_description_en
-, max(indicator_description_ca) as indicator_description_ca
-, max(indicator_description_es) as indicator_description_es
-, max(indicator_description_eu) as indicator_description_eu
-, max(indicator_description_gl) as indicator_description_gl
-, max(indicator_description_nl) as indicator_description_nl
-, is_direct_indicator as is_direct_indicator, max(indicator_category) as indicator_category
-, max(indicator_data_type) as indicator_data_type, max(indicator_unit) as indicator_unit
-, array_agg(
-	case gender when 0 then 'Home'
-		when 1 then 'Dona'
-		when 2 then 'N/B'
-		end
-		ORDER BY gender
-) as gender
-, array_agg(value order by gender) as value
-, count(distinct gender) as num_gender
-, case when count(distinct gender)>0 then '['||string_agg(case gender when 0 then '"Home"'
-		when 1 then '"Dona"'
-		when 2 then '"N/B"'
-		end::varchar,',' order by gender)||']' end as str_gender
-, case when count(distinct gender)>0 then '['||string_agg(value,',' order by gender)||']' else string_agg(value,'') end as str_value
-    , case when count(distinct gender)>0 then '['||string_agg(value,',' order by gender)||']' else string_agg(value,'') end as str_value_en
-    , case when count(distinct gender)>0 then '['||string_agg(value,',' order by gender)||']' else string_agg(value,'') end as str_value_ca
-    , case when count(distinct gender)>0 then '['||string_agg(value,',' order by gender)||']' else string_agg(value,'') end as str_value_es
-    , case when count(distinct gender)>0 then '['||string_agg(value,',' order by gender)||']' else string_agg(value,'') end as str_value_eu
-    , case when count(distinct gender)>0 then '['||string_agg(value,',' order by gender)||']' else string_agg(value,'') end as str_value_gl
-    , case when count(distinct gender)>0 then '['||string_agg(value,',' order by gender)||']' else string_agg(value,'') end as str_value_nl
+    , max(campaign_name) as campaign_name
+    , max(campaign_name_en) as campaign_name_en
+    , max(campaign_name_ca) as campaign_name_ca
+    , max(campaign_name_es) as campaign_name_es
+    , max(campaign_name_eu) as campaign_name_eu
+    , max(campaign_name_gl) as campaign_name_gl
+    , max(campaign_name_nl) as campaign_name_nl
+    , max(campaign_name_fr) as campaign_name_fr
+    , max("year") as "year", max(previous_campaign_id::varchar)::uuid as previous_campaign_id
+    , id_survey, max(survey_created_at) as survey_created_at, max(survey_updated_at) as survey_updated_at
+    , max(status) as status
+    , id_method, bool_or(active) as active
+    , max(method_name) as method_name
+    , max(method_name_en) as method_name_en
+    , max(method_name_ca) as method_name_ca
+    , max(method_name_es) as method_name_es
+    , max(method_name_eu) as method_name_eu
+    , max(method_name_gl) as method_name_gl
+    , max(method_name_nl) as method_name_nl
+    , max(method_name_fr) as method_name_fr
+    , max(method_description) as method_description
+    , max(method_description_en) as method_description_en
+    , max(method_description_ca) as method_description_ca
+    , max(method_description_es) as method_description_es
+    , max(method_description_eu) as method_description_eu
+    , max(method_description_gl) as method_description_gl
+    , max(method_description_nl) as method_description_nl
+    , max(method_description_fr) as method_description_fr
+    , id_user, max(user_name) as user_name, max(user_surname) as user_surname, max(user_email) as user_email
+    , id_organization, max(organization_name) as organization_name, max(vat_number) as vat_number
+    , id_project, max(project_name) as project_name
+    , id_methods_section
+    , max(method_section_title) as method_section_title
+    , max(method_section_title_en) as method_section_title_en
+    , max(method_section_title_ca) as method_section_title_ca
+    , max(method_section_title_es) as method_section_title_es
+    , max(method_section_title_eu) as method_section_title_eu
+    , max(method_section_title_gl) as method_section_title_gl
+    , max(method_section_title_nl) as method_section_title_nl
+    , max(method_section_title_fr) as method_section_title_fr
+    , max(method_order) as method_order, max(method_level) as method_level, max(path_order) as path_order
+    , max(sort_value) as sort_value
+    , id_indicator, indicator_code
+    , max(indicator_name) as indicator_name
+    , max(indicator_name_en) as indicator_name_en
+    , max(indicator_name_ca) as indicator_name_ca
+    , max(indicator_name_es) as indicator_name_es
+    , max(indicator_name_eu) as indicator_name_eu
+    , max(indicator_name_gl) as indicator_name_gl
+    , max(indicator_name_nl) as indicator_name_nl
+    , max(indicator_name_fr) as indicator_name_fr
+    , max(indicator_description) as indicator_description
+    , max(indicator_description_en) as indicator_description_en
+    , max(indicator_description_ca) as indicator_description_ca
+    , max(indicator_description_es) as indicator_description_es
+    , max(indicator_description_eu) as indicator_description_eu
+    , max(indicator_description_gl) as indicator_description_gl
+    , max(indicator_description_nl) as indicator_description_nl
+    , max(indicator_description_fr) as indicator_description_fr
+    , is_direct_indicator as is_direct_indicator, max(indicator_category) as indicator_category
+    , max(indicator_data_type) as indicator_data_type, max(indicator_unit) as indicator_unit
+    , array_agg(
+        case gender when 0 then 'Home'
+            when 1 then 'Dona'
+            when 2 then 'N/B'
+            end
+            ORDER BY gender
+    ) as gender
+    , array_agg(value order by gender) as value
+    , count(distinct gender) as num_gender
+    , case when count(distinct gender)>0 then '['||string_agg(case gender when 0 then '"Home"'
+            when 1 then '"Dona"'
+            when 2 then '"N/B"'
+            end::varchar,',' order by gender)||']' end as str_gender
+    , case when count(distinct gender)>0 then '['||string_agg(value,',' order by gender)||']' else string_agg(value,'') end as str_value
+        , case when count(distinct gender)>0 then '['||string_agg(value,',' order by gender)||']' else string_agg(value,'') end as str_value_en
+        , case when count(distinct gender)>0 then '['||string_agg(value,',' order by gender)||']' else string_agg(value,'') end as str_value_ca
+        , case when count(distinct gender)>0 then '['||string_agg(value,',' order by gender)||']' else string_agg(value,'') end as str_value_es
+        , case when count(distinct gender)>0 then '['||string_agg(value,',' order by gender)||']' else string_agg(value,'') end as str_value_eu
+        , case when count(distinct gender)>0 then '['||string_agg(value,',' order by gender)||']' else string_agg(value,'') end as str_value_gl
+        , case when count(distinct gender)>0 then '['||string_agg(value,',' order by gender)||']' else string_agg(value,'') end as str_value_nl
 from {{ref('answers_calc')}}
-group by id_campaign,  id_survey, id_method, id_user, id_organization
-, id_methods_section, id_indicator, indicator_code, is_direct_indicator
+group by id_campaign,  id_survey, id_method, id_user, id_organization, id_project
+    , id_methods_section, id_indicator, indicator_code, is_direct_indicator
