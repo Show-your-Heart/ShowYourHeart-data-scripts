3c00e09cf2bd3262526c662611670be10666e4cc
Jordi Isidro Llobet <mail@jordiisidrollobet.tech>
2026-07-29 14:58:02 +0200

    si idioma null es posa el per defecte

--- daily/macros/update_answers_calc_agg.sql
+++ daily/macros/update_answers_calc_agg.sql
@@ -19,6 +19,7 @@ select v.id_campaign, v.campaign_name, v."year", v.previous_campaign_id, v.id_su
     , '['||string_Agg('"'||replace(l.title_eu, '"', '')||'"',',')||']' as str_value_eu
     , '['||string_Agg('"'||replace(l.title_gl, '"', '')||'"',',')||']' as str_value_gl
     , '['||string_Agg('"'||replace(l.title_nl, '"', '')||'"',',')||']' as str_value_nl
+    , '['||string_Agg('"'||replace(l.title_fr, '"', '')||'"',',')||']' as str_value_fr
 from vals v join {{ source('dwhpublic', 'syh_methods_listitem')}} l on v.val_id=l.id::text
 group by v.id_campaign, v.campaign_name, v."year", v.previous_campaign_id, v.id_survey, v.survey_created_at, v.survey_updated_at, v.status, v.id_method, v.method_name, v.method_description, v.id_user, v.user_name, v.user_surname, v.user_email, v.id_organization, v.organization_name, v.vat_number, v.id_methods_section, v.method_section_title, v.method_order, v.method_level, v.path_order, v.sort_value, v.id_indicator, v.indicator_code, v.indicator_name, v.indicator_description, v.is_direct_indicator, v.indicator_category, v.indicator_data_type, v.indicator_unit, v.gender, v.value, v.num_gender, v.str_gender
 )
@@ -29,6 +30,7 @@ update {{ this }} set str_value=f.str_value
     , str_value_eu=f.str_value_eu
     , str_value_gl=f.str_value_gl
     , str_value_nl=f.str_value_nl
+    , str_value_fr=f.str_value_fr
 from fin f
 where f.id_campaign={{ this.table }}.id_campaign
 	and f.id_survey={{ this.table }}.id_survey
